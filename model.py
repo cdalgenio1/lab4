27d7947f7bebe475da5975a2c66e44bb5cfa3c50
cdalgenio1 <cdalgenio1@up.edu.ph>
2026-02-27 13:00:45 +0800

    winner

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ class ConnectTacToeModel:
     def __init__(self):
         self._current_player:Player = Player.P1
         self.grid:list[list[str]] = [["."]*7 for _ in range(6)]
-        self.tokens: list = ("1","2")
+        self.tokens: list = ("A","B")
     @property
     def current_player(self)->Player:
         return self._current_player
@@ -14,9 +14,12 @@ class ConnectTacToeModel:
         for token in self.tokens:
             check = token*3
             for r in self.grid:
-                to_check = "".join(r)
-                if check in to_check:
+                to_check_row = "".join(r)
+                if check in to_check_row:
                     return self._current_player
+            check_columns = ["".join([self.grid[_r][c] for _r in range(6)]) for c in range(7)]
+            if check in check_columns:
+                return self._current_player
     @property
     def is_game_done(self)->bool:
         ...
@@ -31,4 +34,4 @@ class ConnectTacToeModel:
     def get_owner(self,row:int,col:int)->Player|None:
         ...
 
-print(ConnectTacToeModel().winner)
+ConnectTacToeModel().winner
