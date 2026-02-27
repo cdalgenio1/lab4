eda0683cc09262753d04567aa18d121d8716d495
Aji-Piyu <altheajaneablola@gmail.com>
2026-02-27 13:31:34 +0800

    Refactor ConnectTacToeView methods and types

--- view.py
+++ view.py
@@ -2,9 +2,6 @@ from common_types import Player
 from collections.abc import Sequence
 
 class ConnectTacToeView:
-    def __init__(self):
-        self.taken = [(-1,1)]
-
     def show_player(self, player: Player):
         if player == Player.P1:
             print("It is currently Player 1's turn")
@@ -19,31 +16,29 @@ class ConnectTacToeView:
             print("".join(row))
         print()
 
-    def ask_for_coords(self) -> Sequence[int] | None :
+    def ask_for_coords(self) -> tuple[int, int]:
         i = -1
         j = -1
-        while (i,j) not in self.taken:
-            while not 0 <= i < 6:
-                try:
-                    i = int(input(f"Choose a row    [0-6]: "))
-                except KeyboardInterrupt:
-                    exit()
-                except:
-                    ...
+        while not 0 <= i < 6:
+            try:
+                i = int(input(f"Choose a row    [0-6]: "))
+            except KeyboardInterrupt:
+                exit()
+            except:
+                ...
 
-            while not 0 <= j < 7:
-                try:
-                    j = int(input(f"Choose a column [0-7]: "))
-                except KeyboardInterrupt:
-                    exit()
-                except:
-                    ...
+        while not 0 <= j < 7:
+            try:
+                j = int(input(f"Choose a column [0-7]: "))
+            except KeyboardInterrupt:
+                exit()
+            except:
+                ...
 
             print()
-            return [i, j]
+            return i, j
 
-
-    def show_winner(self, winner: Player | None, i: int, j: int):
+    def show_winner(self, winner: Player | None):
         if winner == Player.P1:
             print("Congratulations, Player 1!, You have officially won!")
         elif winner == Player.P2:
