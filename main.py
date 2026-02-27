d47c42f09f1c7bf5a245233c5f339bfbce256cfd
Aji-Piyu <altheajaneablola@gmail.com>
2026-02-27 12:47:02 +0800

    hello I add main ontrler

---
+++ main.py
@@ -0,0 +1,39 @@
+from model import ConnectTacToeModel
+from view import ConnectTacToeView
+from common_types import Player
+
+class ConnectTacToeController:
+    def __init__(self, model: ConnectTacToeModel, view: ConnectTacToeView):
+        self._model = model
+        self._view = view
+
+    def start(self):
+        model = self._model
+        view = self._view
+
+        while not model.is_game_done():
+            #model.start_turn()
+            if model.current_player == Player.P1:
+                view.show_grids(model.grid)
+                view.show_player(model.curent_player)
+                i, j = view.ask_for_coords()#input what it asls
+                model.choose_cell(i, j)
+            
+            else:
+                i, j = model.get_random_coords()
+                model.choose_cell(i, j)
+        
+        if model.winner() is not None:
+            print(f'player {model.winner()} won !')
+        else:
+            print('Both players draw !')
+
+
+                
+if __name__ == "__main__":
+    row_count = 6
+    col_count = 7
+    view = ConnectTacToeView()
+    model = ConnectTacToeModel()
+    controller = ConnectTacToeController(model, view)
+    controller.start()
